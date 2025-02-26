90e764ba91ac980dba666378c05e140e2b6abe90
Allan Joel Baque Jacome <baquea@ecuador.h.local>
2025-02-26 10:48:51 -0500

    agregando info inve

--- src/app/pages/my-store/inventario/inventario.component.ts
+++ src/app/pages/my-store/inventario/inventario.component.ts
@@ -1,46 +1,39 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { Router, RouterModule } from '@angular/router';
+import {  RouterModule } from '@angular/router';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzTableModule } from 'ng-zorro-antd/table';
+import { InventarioService } from '../../../services/inventario.service';
+import { LoadingService } from '../../../services/loading.service';
+import { ModalResponseService } from '../../../services/modal-response.service';
+import { Product } from '../../../interfaces/product';
+import { environment } from '../../../../environments/environment';
+import { NzImageModule } from 'ng-zorro-antd/image';
 
 @Component({
   selector: 'app-inventario',
-  imports: [ NzButtonModule,ReactiveFormsModule, NzInputModule, NzFormModule,NzTableModule,RouterModule],
+  imports: [ NzButtonModule,ReactiveFormsModule, NzInputModule, NzFormModule,NzTableModule,RouterModule, NzImageModule],
   templateUrl: './inventario.component.html',
   styleUrl: './inventario.component.css'
 })
 
 export class InventarioComponent {
     form!:FormGroup;
-    
-    constructor(private fb:FormBuilder){
+    dataSet: Product[] = [];
+    url:string = environment.Url
+    constructor(private fb:FormBuilder,private modalService:ModalResponseService, private inventarioService:InventarioService, private loadinService:LoadingService){
       this.form = fb.group({
           search: fb.control(''),
       })
+      this.loadinService.loadingOn()
+      this.inventarioService.getProducts().subscribe({
+        next:(data)=>{this.dataSet = data.data},
+        error:err=>this.modalService.setModalResponse={status:'error',title:"Error al obtener datos",description:err},
+        complete:()=>this.loadinService.loadingOff()
+      })
     }
-    dataSet: any = [
-      {
-        key: '1',
-        name: 'John Brown',
-        age: 32,
-        address: 'New York No. 1 Lake Park'
-      },
-      {
-        key: '2',
-        name: 'Jim Green',
-        age: 42,
-        address: 'London No. 1 Lake Park'
-      },
-      {
-        key: '3',
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sidney No. 1 Lake Park'
-      }
-    ];
     submitForm(){
 
     }
